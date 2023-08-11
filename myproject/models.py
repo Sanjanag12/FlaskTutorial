8541ded129e8712db23ddba298848888b3572c22
Sanjana Shetty <134592355+Sanjanag12@users.noreply.github.com>
2023-08-11 13:07:12 +0530

    modified

--- myproject/models.py
+++ myproject/models.py
@@ -23,3 +23,12 @@ class Owner(db.Model):
     __tablename__ ='owners'
     id =db.Column(db.Integer , primary_key=True)
     name=db.Column(db.Text)
+
+    puppy_id = db.Column(db.Integer,db.ForeignKey('puppies.id'))
+
+    def __init__(self,name,puppy_id):
+        self.name=name
+        self.puppy_id=puppy_id
+
+    def __repr__(self):
+        return f"Owner Name:{self.name}"
