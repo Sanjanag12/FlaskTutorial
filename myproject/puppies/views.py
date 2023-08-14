94888f4d71709f805801174adacbb1e55b8036dc
Sanjana Shetty <134592355+Sanjanag12@users.noreply.github.com>
2023-08-14 10:19:53 +0530

    modified

--- myproject/puppies/views.py
+++ myproject/puppies/views.py
@@ -6,3 +6,37 @@ from myproject.puppies.forms import AddForm , DelForm
 
 puppies_blueprint =Blueprint('puppies', __name__,
                              template_folder='templates/puppies')
+
+@puppies_blueprint.route('/add', methods=['GET','POST'])
+
+def add():
+    form=AddForm()
+
+    if form.validate_on_submit():
+        name=form.name.data
+
+        new_pup =Puppy(name)
+        db.session.add(new_pup)
+        db.session.commit()
+
+        return redirect(url_for('puppies.list'))
+    return render_template('add.html', form=form)
+
+
+def list():
+    puppies=Puppy.query.all()
+    return render_template('list.html',puppies=puppies)
+
+def delete():
+    form =DelForm()
+
+    if form.validate_on_submit():
+        id=form.id.data
+        pup=Puppy.query.get(id)
+        db.session.delete(pup)
+        db.session.commit()
+
+        return redirect(url_for('puppies.list'))
+    return render_template('delete.html',form=form)
+
+
